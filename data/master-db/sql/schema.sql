07b450681ed4a121e8e43952eb0d426da78d551a
SimplyAMan <simplyaman@gmail.com>
2023-12-04 14:00:01 +0200

    Remove directories for database from repository and fix script for master-db

--- data/master-db/sql/schema.sql
+++ data/master-db/sql/schema.sql
@@ -1,9 +1,9 @@
 --drop table result;
 
-create table if not exists result
+create table if not exists document
 (
     document_id bigint,
     created     timestamp default now(),
     constraint pk_document primary key (document_id)
 );
--- ALTER TABLE result REPLICA IDENTITY FULL;
+-- ALTER TABLE document REPLICA IDENTITY FULL;
